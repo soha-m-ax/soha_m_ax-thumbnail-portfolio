4a9d1f3518c1865d9436c7554be2c158e5c56489
soha-m-ax <senapatisoham12@gmail.com>
2025-03-23 00:06:54 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("view-thumbnails").addEventListener("click", function() {
-    let gallery = document.getElementById("gallery");
-    if (gallery.style.display === "none" || gallery.style.display === "") {
-        gallery.style.display = "flex";
-    } else {
-        gallery.style.display = "none";
-    }
-});
